9faa4a0af85b6f188085e4dac5b8d604d52bc1c4
ArakakiTakuya <225arataku@gmail.com>
2020-06-08 17:57:22 +0900

    set view point

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("App listening on port" + port);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
