3d6519f4b22a9a680f44d9cd76b0691e83f9e793
ArakakiTakuya <225arataku@gmail.com>
2020-06-09 10:54:07 +0900

    set endpoints for GET

--- server.js
+++ server.js
@@ -2,14 +2,42 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const db = require("./knex.js");
+
 app.use(express.static(path.resolve(__dirname, "build")));
+
+app.get("/api/words", async (req, res) => {
+  try {
+    const words = await db("words").select();
+    res.status(200).json({ words: words });
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).end();
+  }
+});
+
+app.get("/api/words/:level", async (req, res) => {
+  const level = req.params.level;
+  try {
+    if (level === "basic") {
+      const words = await db("words").where("level", "=", "basic").select();
+      res.status(200).json({ words: words });
+    } else if (level === "advance") {
+      const words = await db("words").where("level", "=", "advance").select();
+      res.status(200).json({ words: words });
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).end();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("App listening on port" + port);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
